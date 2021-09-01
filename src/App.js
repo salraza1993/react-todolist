f334c3ea13be5a4a44a33964f456058b286c1a58
Salman Ahmad <86913182+salAhamad@users.noreply.github.com>
2021-09-01 18:00:55 +0400

    todo first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
 function App() {
+
+  // https://www.youtube.com/watch?v=E1E08i2UJGI
+  let todoArray = [];
+  const [input, setInput] = useState('');
+
+  const changeHandler = (e) => setInput(e.target.value);
+
+  const onSubmitData = () => {
+    setInput()
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="inputContainer">
+      <h2>Add ToDo list</h2>
+      <br />
+      <form action="" onSubmit={() => onSubmitData}>
+        <input type="text" id="inputBar" placeholder="enter your toDo heading" onChange={()=> changeHandler} />
+        <textarea name="" id="todoContent" cols="30" rows="5" onChange={()=> changeHandler} ></textarea>
+        <button id="addButton">Add ToDo</button>
+      </form>
     </div>
-  );
+  )
 }
 
 export default App;
